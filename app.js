c75e0f60cd83faf28de1952bd51f12aa47798d10
KillerDucks <nimesh.patel@bindserver.com>
2018-12-28 11:50:15 +0000

    Added Client + Single API routes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const ws = require('ws');
 
 const Structs = require("./structs");
 const Config = require("./configs");
-const DB = require("./db");
+const DB = require("./DB_Class");
 
 // Configs
 const wss = new ws.Server({ port: 8080 });
 const app = express();
 app.use("/assets", express.static(__dirname + "/views/assets"));
+const dbConn = new DB("localhost", "WorkScheduler", "Jobs");
 
 // Globals
 const port = 3000;
@@ -20,25 +21,19 @@ wss.on('connection', function connection(ws) {
         console.log('[WebSocket::Message]\treceived: %s', message);
     });
 
-    let sampleJob = new Structs.Job("WorkScheduler", "Make the App!!!", "10 Weeks", 10, new Structs.Client("KillerDucks", "killerducks@bindserver.com", "sad67as3"));
-    let debugMessage = new Structs.WsMessage("Job", false, sampleJob);
-    ws.send(JSON.stringify(debugMessage));
+    // Debugging
+    dbConn.JSONResult((data) => {
+        data.forEach(job => {
+            let debugMessage = new Structs.WsMessage("Job", false, job);
+            ws.send(JSON.stringify(debugMessage));     
+        });
+    });
 });
+
+////////////////////// FrontEnd Routes //////////////////////
 
 // FrontEnd Page
 app.get('/', (req, res) => {
-    console.log(`[Express::/]\tServed / to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
-    res.sendFile(__dirname + "/views/index.html");    
-});
-
-// Get all Jobs
-app.get('/jobs', (req, res) => {
-    console.log(`[Express::/]\tServed / to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
-    res.sendFile(__dirname + "/views/index.html");    
-});
-
-// Get a Job (Using an ID)
-app.get('/job/:jobID', (req, res) => {
     console.log(`[Express::/]\tServed / to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
     res.sendFile(__dirname + "/views/index.html");    
 });
@@ -48,6 +43,42 @@ app.get('/debug', (req, res) => {
     console.log(`[Express::/]\tServed /debug to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
     res.sendFile(__dirname + "/views/debugJS.html");    
 });
+
+////////////////////// API Routes //////////////////////
+
+// Get all Jobs
+app.get('/jobs', (req, res) => {
+    console.log(`[Express::/]\tServed /jobs to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`);
+    dbConn.JSONResult((data) => {
+        res.status(200).json(data);
+    });    
+});
+
+// Get a Job (Using an ID)
+app.get('/job/:jobID', (req, res) => {
+    console.log(`[Express::/]\tServed /job/:jobID to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
+    dbConn.JSONResultSingle(req.params.jobID, (data) => {
+        res.status(200).json(data);
+    });  
+});
+
+// Get all Clients
+app.get('/clients', (req, res) => {
+    console.log(`[Express::/]\tServed /clients to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`);
+    dbConn.JSONResult((data) => {
+        res.status(200).json(data);
+    }, "Clients");    
+});
+
+// Get a Client (Using an ID)
+app.get('/client/:clientID', (req, res) => {
+    console.log(`[Express::/]\tServed /client/clientID to: [${req.connection.remoteFamily}] ${req.connection.remoteAddress}:${req.connection.remotePort}`); 
+    dbConn.JSONResultSingle(req.params.clientID, (data) => {
+        res.status(200).json(data);
+    }, "Clients");  
+});
+
+////////////////////// Express Listen //////////////////////
 
 // Listen on Port and Serve Client
 app.listen(port, () => {
